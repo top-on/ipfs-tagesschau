05b8c46681b3b1e964aa95a5069873864b82a555
top-on <top-on@posteo.de>
2022-06-28 19:45:07 +0200

    cache previously downloaded articles

--- ipfs_tagesschau/io.py
+++ ipfs_tagesschau/io.py
@@ -2,12 +2,13 @@
 
 import requests
 from urlpath import URL
+from pathlib import Path
 
 
 def download_page_source(url: URL) -> str:
     return requests.get(url).text
 
 
-def save_rendering(rendering: str, filename: str):
-    with open(f"build/{filename}", "w") as f:
+def save_rendering(rendering: str, filepath: Path):
+    with open(str(filepath), "w") as f:
         f.write(rendering)
--- scripts/scrape_feed.py
+++ scripts/scrape_feed.py
@@ -1,8 +1,9 @@
 """Scrape ATOM feed of tagesschau.de."""
 
-import shutil
 import time
+from datetime import datetime
 from urlpath import URL
+from pytz import timezone
 
 from ipfs_tagesschau.config import OUT_DIR
 from ipfs_tagesschau.io import download_page_source, save_rendering
@@ -10,24 +11,30 @@ from ipfs_tagesschau.parse import parse_article, parse_feed
 from ipfs_tagesschau.render import render_article, render_feed
 
 
-# RESET OUTPUT FOLDER
-shutil.rmtree(OUT_DIR)
+# CREATE OUTPUT FOLDER
+timestamp = datetime.now(tz=timezone("Europe/Berlin")).isoformat()
 OUT_DIR.mkdir(exist_ok=True)
 
 # RENDER FEED
 page_source = download_page_source(url=URL("https://www.tagesschau.de/xml/atom/"))
 summaries = parse_feed(page_source=page_source)
 rendering = render_feed(articles=summaries)
-save_rendering(rendering=rendering, filename="index.html")
+save_rendering(rendering=rendering, filepath=OUT_DIR / "index.html")
 
 # DOWNLOAD ARTICLES
 for i, article_summary in enumerate(summaries):
+
+    filepath = OUT_DIR / article_summary.url.name
+
+    if filepath.is_file():
+        print(f"Skipping article {i+1}/{len(summaries)}. File already exists.")
+        continue
 
     print(f"Loading article {i+1}/{len(summaries)} ({article_summary.url.name})...")
 
     page_source = download_page_source(url=article_summary.url)
     article = parse_article(page_source=page_source)
     rendering = render_article(article=article)
-    save_rendering(rendering=rendering, filename=article_summary.url.name)
+    save_rendering(rendering=rendering, filepath=filepath)
 
     time.sleep(3)
